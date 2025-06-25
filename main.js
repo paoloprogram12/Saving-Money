b28df9dd869124f69cf197240a4f75a6e43e8b8f
paoloprogram12 <sethpsalazar@gmail.com>
2025-06-24 17:59:56 -0700

    added one more question

--- main.js
+++ main.js
@@ -5,12 +5,20 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// questions to ask user for saving money
 rl.question("Enter the amount of money you want to save up to: ", function(moneyGoal) {
   console.log("Your goal is to save: $" + moneyGoal);
 
   rl.question("Enter in how many days you want to reach this goal: ", function(numDays) {
     console.log("You want to obtain $" + moneyGoal + " in " + numDays + " days.");
 
-    rl.close();
+    rl.question("Enter in how often you want to make deposits (1 for Weeks, 2 for Months, 3 for Days): ", function(depositOption) {
+        if (depositOption == 1) { console.log("You chose every week"); }
+        else if (depositOption == 2) { console.log("You chose every month"); }
+        else if (depositOption == 3) { console.log("You chose every day"); }
+
+        rl.close();
+    });
   });
 });
+

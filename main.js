97b70146ccf1f2e03968ce3ebac2d2f48cf9fdc9
paoloprogram12 <sethpsalazar@gmail.com>
2025-06-24 17:52:39 -0700

    basic input format, user will be asked these two questions once opening website

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-let moneyGoal = prompt("Enter the amount of money you want to save up to:")
-console.log("So you want to save up to " + moneyGoal + "?");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Enter the amount of money you want to save up to: ", function(moneyGoal) {
+  console.log("Your goal is to save: $" + moneyGoal);
+
+  rl.question("Enter in how many days you want to reach this goal: ", function(numDays) {
+    console.log("You want to obtain $" + moneyGoal + " in " + numDays + " days.");
+
+    rl.close();
+  });
+});

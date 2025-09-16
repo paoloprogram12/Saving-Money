c05efc852433bfc49d3dfbb64f7d050bc04d69df
paoloprogram12 <sethpsalazar@gmail.com>
2025-09-16 15:17:47 -0700

    signup backend

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,37 @@ const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
+// throws error if db is unreachable
+db.connect(err => {
+    // throws error if db is unreachable
+    if (err) { console.error('DB connect error', err); process.exit(1); }
+    console.log('Connected to MySQL');
+});
 
+// creates signup
+app.post('/signup', async(req, res) => {
+    // grabs credentials from body
+    const { username, email, password} = req.body;
+
+    // checks if credentials are filled
+    if (!username || !email || !password) {
+        return res.status(400).send('All fields are required');
+    }
+
+    // ensures credentials are unique (user and email)
+    db.query('SELECT 1 FROM users WHERE username=? OR email=?', [username, email], async (err, rows) => {
+        if (err) { return res.status(500).send('Server Error'); }
+        if (rows.length) { return res.status(400).send('Username or email already taken'); }
+
+        const hash = await bcrypt.hash(password, 10);
+
+        db.query(
+            'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+            [username, email, hash],
+            (err2) => {
+                if (err) { return res.status(500).send('Error creating user'); }
+                res.send('Signup successful')
+            }
+        );
+    });
+});

92e1698af06e3f656649afef5935b466e572f76e
paoloprogram12 <sethpsalazar@gmail.com>
2025-09-16 14:55:44 -0700

    connect to the mysql db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-const express = require('express');
-const msql = require('mysql2');
-const bcrypt = require('bcryptjs');
+const express = require('express'); // web framework to create routes
+const mysql = require('mysql2'); // SQL db
+const bcrypt = require('bcryptjs'); // used to hash out and compare passwords
 const cors = require('cors'); // allows which frontends are allowed to send requests to backend
 
 const app = express();
@@ -11,3 +11,12 @@ const port = process.env.port || 5001;
 app.use(cors({ origin: 'http://localhost:3000'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// db connection
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
